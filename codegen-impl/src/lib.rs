a0da8e06e7d3dc00155fc2ddf7533eff2dc522b8
Adam H. Leventhal <ahl@oxide.computer>
2025-01-22 17:06:04 -0800

    Update the template with our updated preferences

--- codegen-impl/src/lib.rs
+++ codegen-impl/src/lib.rs
@@ -16,11 +16,24 @@ pub fn do_codegen() -> TokenStream {
 mod tests {
     use crate::do_codegen;
 
+    // You can write simple tests that validate output, but those often require
+    // weird formatting that can be challenging to get right.
     #[test]
-    fn test() {
+    fn simple_test() {
         assert_eq!(
             do_codegen().to_string(),
             r#"fn print_hi () { println ! ("I guess this is working!") ; }"#
         );
+    }
+
+    // The expectorate crate makes for more comprehensible tests that are also
+    // easier to review. When code generation changes, expectorate makes it
+    // easy to update fixtures based on the new code.
+    #[test]
+    fn robust_test() {
+        let code = do_codegen();
+        let file = syn::parse2(code).expect("invalid codegen");
+        let actual = prettyplease::unparse(&file);
+        expectorate::assert_contents("tests/data/robust_test.rs", &actual);
     }
 }
---
+++ codegen-impl/tests/data/robust_test.rs
@@ -0,0 +1,3 @@
+fn print_hi() {
+    println!("I guess this is working!");
+}
