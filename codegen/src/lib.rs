945df7f7032a279b24fce628fac945cf4533c136
Adam Leventhal <ahl@oxide.computer>
2025-01-22 17:14:51 -0800

    Update the template with our updated preferences (#3)

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -8,14 +8,13 @@ pub use codegen_macro::*;
 // for use with a build.rs script.
 use codegen_impl::do_codegen;
 
-use rustfmt_wrapper::rustfmt;
-
 /// This is what we'd expect build.rs scripts to call if they wanted to use our
 /// code generator to create files rather than as macros. While one can see the
 /// output from macros using `cargo expand` it's much easier if the code is
-/// placed in a file. To make things even easier, we run the TokenStream
-/// through rustfmt first.
+/// placed in a file. To make things even easier to understand, we run the
+/// TokenStream through prettypretty please to format the code first.
 pub fn build() -> String {
     let code = do_codegen();
-    rustfmt(code).unwrap()
+    let parsed = syn::parse2(code).expect("invalid codegen");
+    prettyplease::unparse(&parsed)
 }
